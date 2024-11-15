b5922e22349ac952e8c897030c5d9a98b4ccdf06
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-11-15 21:08:47 +0000

    new cleark

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.//
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ["/", "/api/(.*)", "/sign-in", "/sign-up"],
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    //  "/(api|trpc)(.*)",
-    // Match all API routes
-    "/api/(.*)",
-    // "/(api|trpc)(?!/public-api)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
