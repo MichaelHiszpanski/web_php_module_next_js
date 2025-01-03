f683385b4237beafecbceaedab9b67b46c10d010
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2025-01-03 14:52:07 +0000

    clerk version

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 ---
 # The project is being carried out as part of the requirements for passing the Web Design course at The Open University.
 ---
+
 ## Deployed in Vercel
 
-Link: [Check out the Project](https://schoolmanager-mu.vercel.app/)     Desktop version only
-
+Link: [Check out the Project](https://schoolmanager-mu.vercel.app/) Desktop version only
 
 ---
-
 
 ## Technologies Used
 
 ### **PostgreSQL**
+
 PostgreSQL is a powerful, open-source object-relational database system known for its reliability and advanced features. It is used in this project for managing structured data efficiently. The database handles core data operations like storing user information, application settings, and other essential details.
 
 ### **TanStack Query**
+
 TanStack Query (formerly React Query) is used to manage server-state in the project. It simplifies data fetching, caching, and synchronization with the backend, ensuring a seamless user experience while reducing boilerplate code for API integration.
 
 ### **Clerk for Authentication**
+
 Clerk is implemented in this project to handle user authentication and management. It provides features like secure sign-in/sign-up, session handling, and user profile management, ensuring robust security for the application.
 
 ---
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,30 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware } from "@clerk/nextjs/server";
+// Old Version "@clerk/nextjs": "^4.29.6",
+// // This example protects all routes including api/trpc routes
+// // Please edit this to allow other routes to be public as needed.//
+// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// export default authMiddleware({
+//   publicRoutes: ["/", "/api/(.*)", "/sign-in", "/sign-up"],
+// });
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.//
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: ["/", "/api/(.*)", "/sign-in", "/sign-up"],
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+//New version   "@clerk/nextjs": "^6.9.6",
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/profile(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
