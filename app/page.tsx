f031c167ae1b88ee28157428edb3732cf2b575c0
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2025-01-05 20:10:14 +0000

    deleting

--- app/page.tsx
+++ app/page.tsx
@@ -21,25 +21,6 @@ const Home: NextPage = ({}) => {
       userStore.clearUser();
     }
   }, []);
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const res = await fetch("/api/create-db", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ comment }),
-    });
-
-    const data = await res.json();
-
-    if (res.ok) {
-      setMessage(data.message);
-    } else {
-      setMessage(data.error || "Something went wrong");
-    }
-  };
 
   return (
     <div>
@@ -221,18 +202,6 @@ const Home: NextPage = ({}) => {
               </p>
             </div>
           </div>
-          {/* <div className="bg-white mb-[200px]">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="Write a comment"
-                name="comment"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-              />
-              <button type="submit">Submit</button>
-            </form>
-          </div> */}
 
           {message && <p>{message}</p>}
         </div>
