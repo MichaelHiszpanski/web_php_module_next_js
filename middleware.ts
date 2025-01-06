c0dc10100460f684d4cb9a0303b6f8c117d25697
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2025-01-06 00:10:21 +0000

    changing clerk version

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,12 @@
-// import { authMiddleware } from "@clerk/nextjs/server";
-// Old Version "@clerk/nextjs": "^4.29.6",
-// // This example protects all routes including api/trpc routes
-// // Please edit this to allow other routes to be public as needed.//
-// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-// export default authMiddleware({
-//   publicRoutes: ["/", "/api/(.*)", "/sign-in", "/sign-up"],
-// });
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-//New version   "@clerk/nextjs": "^6.9.6",
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/profile(.*)"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+import { authMiddleware } from "@clerk/nextjs/server";
+//Old Version "@clerk/nextjs": "^4.29.6",
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.//
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ["/", "/api/(.*)", "/sign-in", "/sign-up"],
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
